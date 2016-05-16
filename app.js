1087063b5e34981a5f1a178cd5e03dc2b5e0fbbf
Eric Grivel <github-com@lumenssolutions.com>
2016-05-16 19:33:31 -0400

    match esling

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
-const http         = require('http'),
-      fs           = require('fs'),
-      path         = require('path'),
-      contentTypes = require('./utils/content-types'),
-      sysInfo      = require('./utils/sys-info'),
-      env          = process.env;
 
-let server = http.createServer(function (req, res) {
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const contentTypes = require('./utils/content-types');
+const sysInfo = require('./utils/sys-info');
+const env = process.env;
+
+let server = http.createServer(function(req, res) {
   let url = req.url;
-  if (url == '/') {
+  if (url === '/') {
     url += 'index.html';
   }
 
+  console.log('Got URL: ' + url);
   // IMPORTANT: Your application HAS to respond to GET /health with status 200
   //            for OpenShift health monitoring
-
   if (url === '/health') {
     res.writeHead(200);
     res.end();
@@ -26,7 +27,7 @@ let server = http.createServer(function (req, res) {
     res.writeHead(200);
     res.end('<h1>Hello, other world!</h1>');
   } else {
-    fs.readFile('./static' + url, function (err, data) {
+    fs.readFile('./static' + url, function(err, data) {
       if (err) {
         res.writeHead(404);
         res.end();
@@ -42,6 +43,6 @@ let server = http.createServer(function (req, res) {
   }
 });
 
-server.listen(env.NODE_PORT || 3000, env.NODE_IP || 'localhost', function () {
+server.listen(env.NODE_PORT || 3000, env.NODE_IP || 'localhost', function() {
   console.log(`Application worker ${process.pid} started...`);
 });
