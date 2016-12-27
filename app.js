09a2929f69ff54f98a7ba287a3727d7ce1199759
Eric Grivel <github-com@lumenssolutions.com>
2016-12-27 17:38:02 -0500

    Updated IP address

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ let server = http.createServer(function(req, res) {
       }
     });
   } else if (url.indexOf('/ph') === 0) {
-    var remoteIP = '71.121.234.125';
+    var remoteIP = '71.121.239.232';
     var remotePort = 4433;
     var remotePrefix = '/cgi-bin/photos';
     var options = {
